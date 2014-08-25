a6bd9ace5c91becbeaad719905549772d1bcc161
Mo Dong <montedong@gmail.com>
2014-08-25 07:56:15 -0700

    update the demo scripts

--- tune_bw_rtt_loss.sh
+++ tune_bw_rtt_loss.sh
@@ -1,4 +1,4 @@
-ssh -t -t -o StrictHostKeyChecking=no modong2@bridge0.demopair1.uiucscheduling.emulab.net "~/setpipe.sh $1 $2 $3 $4"
+ssh -t -t -o StrictHostKeyChecking=no modong2@bridge0.demopair1.uiucscheduling.emulab.net "~/setpipe.sh $1 $2 $3 $4 $5 $6"
 ssh -t -t -o StrictHostKeyChecking=no modong2@receiver1.demopair1.uiucscheduling.emulab.net "~/setloss.sh $8"
 ssh -t -t -o StrictHostKeyChecking=no modong2@bridge0.demopair2.uiucscheduling.emulab.net "~/setpipe.sh $1 $2 $3 $4 $5 $6"
 ssh -t -t -o StrictHostKeyChecking=no modong2@receiver1.demopair2.uiucscheduling.emulab.net "~/setloss.sh $8"
